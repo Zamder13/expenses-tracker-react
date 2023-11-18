1a2d99632257067b0605cf42e23afbeccda82072
BlackSwift <Zamder@ukr.net>
2023-11-18 14:48:05 +0200

    add UIcss for single transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
   return (
-    <li className="minus">
-      {transaction.text} <span>-$400</span>
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {/* do something interesting with {transaction amount}, delete negative value:
+        The Math.abs() static method returns the ABSOLUTE value of a number.
+        */}
+        {sign}${Math.abs(transaction.amount)}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   );
