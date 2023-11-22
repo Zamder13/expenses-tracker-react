a4d286912a5f5e207b340ce664ff987ccf9cc971
BlackSwift <Zamder@ukr.net>
2023-11-22 08:14:10 +0200

    start BD with troubles

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db.js");
 
 dotenv.config({ path: "./config/config.env" });
+// connectDB();
 
 const transactions = require("./routes/transactions");
 
 const app = express();
 
-// app.get("/", (req, res) => res.send("Hello!"));
 app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 7000;
