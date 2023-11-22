bb2e3ff6f7477bd13cd25ca1db9085025f8e845d
BlackSwift <Zamder@ukr.net>
2023-11-22 20:25:10 +0200

    test commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(
       `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
@@ -15,7 +12,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
-
 
 module.exports = connectDB;
