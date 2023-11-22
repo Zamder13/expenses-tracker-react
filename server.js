fd0f3ca3bddabab4ff881f1a5974d329123339f2
saske <swiftender@gamil.com>
2023-11-22 15:49:00 +0200

    add method and they are working, also change REMOVE for DELETEONE

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const colors = require("colors");
-const morgan = require("morgan");
-const connectDB = require("./config/db.js");
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+const connectDB = require('./config/db.js')
 
-dotenv.config({ path: "./config/config.env" });
-connectDB();
+dotenv.config({ path: './config/config.env' })
 
-const transactions = require("./routes/transactions");
+connectDB()
 
-const app = express();
+const transactions = require('./routes/transactions')
 
-app.use("/api/v1/transactions", transactions);
+const app = express()
 
-const PORT = process.env.PORT || 7000;
+app.use(express.json())
+
+app.use('/api/v1/transactions', transactions)
+
+const PORT = process.env.PORT || 7000
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold,
+  ),
+)
